d82b2a2cf12dae6e081f8f4133e8840143462f00
maxis55 <maxis.55.m@gmail.com>
2018-06-22 12:00:33 +0300

    Changed media interaction with files

--- views/site/news.php
+++ views/site/news.php
@@ -88,20 +88,15 @@ $this->params['breadcrumbs'][] = $this->title;
                         </div>
                         <?php if (null != $model->media): ?>
                             <?php
-                            $newImageName=$model->media->name;
+                            $imgSrc=$model->media->getImageOfSize(358,176);
                             $newImageAlt=$model->media->alt;
-                            if(file_exists(Yii::getAlias('@web'). 'uploads/images/'.$newImageName) || file_exists(Yii::getAlias('@web'). 'uploads/images/358x176/'.$newImageName)){
-                                if(! file_exists(Yii::getAlias('@web'). 'uploads/images/358x176/'.$newImageName) ){
-                                    if (!is_dir(Yii::getAlias('@web'). 'uploads/images/358x176/')) {
-                                        mkdir(Yii::getAlias('@web'). 'uploads/images/358x176/', 0777, true);
-                                    }
-                                    (Image::getImagine()->open(Yii::getAlias('@web') . 'uploads/images/'.$newImageName)->thumbnail(new Box(358, 176))->save(Yii::getAlias('@web'). 'uploads/images/358x176/'.$newImageName , ['quality' => 90]));
-                                }
+                            if(null != $imgSrc && '' != $imgSrc ){
+
 
                                 echo '<div class="news__pic">';
                                 echo ' <a href="#">';
 
-                                echo '<img style="height:176px;" src="' . Yii::getAlias('@web/' . 'uploads/images/358x176/' . $newImageName) . '" alt="' . $model->media->alt . '">';
+                                echo '<img src="' .$model->media->getImageOfSize(358,176) . '" alt="' . $model->media->alt . '">';
 
                                 echo '</a>';
                                 echo '</div>';
