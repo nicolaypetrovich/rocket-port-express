743ff6a4803ec627a7215213ac83d0404819334c
maxis55 <maxis.55.m@gmail.com>
2018-07-12 12:20:15 +0300

    fixed assets

--- modules/admin/assets/LoginAsset.php
+++ modules/admin/assets/LoginAsset.php
@@ -11,20 +11,20 @@ use yii\web\AssetBundle;
 
 class LoginAsset extends AssetBundle
 {
-    public $sourcePath = '@app/modules/admin/';
+//    public $sourcePath = '@app/modules/admin/';
     public $css = [
-        'bower_components/bootstrap/dist/css/bootstrap.min.css',
-        'bower_components/font-awesome/css/font-awesome.min.css',
-        'bower_components/Ionicons/css/ionicons.min.css',
-        'dist/css/AdminLTE.min.css',
-        'dist/css/skins/skin-blue.min.css',
+        'adminparts/bower_components/bootstrap/dist/css/bootstrap.min.css',
+        'adminparts/bower_components/font-awesome/css/font-awesome.min.css',
+        'adminparts/bower_components/Ionicons/css/ionicons.min.css',
+        'adminparts/dist/css/AdminLTE.min.css',
+        'adminparts/dist/css/skins/skin-blue.min.css',
         'https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic',
 
     ];
     public $js = [
 //        'bower_components/jquery/dist/jquery.min.js',
-        'bower_components/bootstrap/dist/js/bootstrap.min.js',
-        'plugins/iCheck/icheck.min.js',
+        'adminparts/bower_components/bootstrap/dist/js/bootstrap.min.js',
+        'adminparts/plugins/iCheck/icheck.min.js',
 //        '/js/twitter-text.js',
 //        '/js/twitter_count.js',
 //        '/js/status-counter.js',
