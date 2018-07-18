0747faff9e82c183363735219835f47cd3438893
Unknown <gavreel911@gmail.com>
2018-07-18 16:05:09 +0300

    footer fix

--- modules/admin/views/layouts/main.php
+++ modules/admin/views/layouts/main.php
@@ -144,17 +144,6 @@ scratch. This page gets rid of all links and provides the needed markup only.
     </div>
     <!-- /.content-wrapper -->
 
-    <!-- Main Footer -->
-    <footer class="main-footer">
-        <!-- To the right -->
-        <div class="pull-right hidden-xs">
-            Anything you want
-        </div>
-        <!-- Default to the left -->
-        <strong>Copyright &copy; 2016 <a href="#">Company</a>.</strong> All rights reserved.
-    </footer>
-
-
 </div>
 <div id="MediaLibrary" class="modalMediaLibrary">
 
